69e6938fca23bd1d8b4072a9d592b321b9fdfa9f
Aaron To <AaronTo@macintosh-4.wifi.local.cmu.edu>
2022-11-01 20:50:40 -0400

    Added tests to deal with batch students

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -1,5 +1,8 @@
-from flask import Flask
+#  import module_manager
+# module_manager.review()
 
+from flask import Flask
+import pytest
 from app.handlers.routes import configure_routes
 
 
@@ -13,3 +16,75 @@ def test_base_route():
 
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
+
+def test_batch_success():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    # Values will be taking a parameter string with _ as sep. variables and , as sep. students
+    url = '/batch?18_19_3,20_10_4'
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert response.get_data() == b'2\n'
+
+def test_batch_failed_no_students():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    # Values will be taking a parameter string with _ as sep. variables and , as sep. students
+    url = '/batch'
+
+    response = client.get(url)
+
+    assert response.status_code == 500
+    assert response.get_data() == b'\n'
+    
+def test_batch_failed_incorrect_bounds():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    # Values will be taking a parameter string with _ as sep. variables and , as sep. students
+    url = '/batch?18_19_3,20_10000_4'
+
+    response = client.get(url)
+
+    assert response.status_code == 500
+    assert response.get_data() == b'\n'
+
+def test_batch_failed_too_many_params():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    # Values will be taking a parameter string with _ as sep. variables and , as sep. students
+    url = '/batch?18_19_3,20_10_4_10'
+
+    response = client.get(url)
+
+    assert response.status_code == 500
+    assert response.get_data() == b'\n'
+
+def test_batch_failed_not_enough_params():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    # Values will be taking a parameter string with _ as sep. variables and , as sep. students
+    url = '/batch?18_19_3,20_10_'
+
+    response = client.get(url)
+
+    assert response.status_code == 500
+    assert response.get_data() == b'\n'
+
+def test_batch_one_student():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    # Values will be taking a parameter string with _ as sep. variables and , as sep. students
+    url = '/batch?18_19_3'
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert response.get_data() == b'1\n'
